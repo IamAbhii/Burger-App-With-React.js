f48628a0eb822a46cc95d6118c3cce414a9edef3
abhi <abrahmbhatt03@gmai.com>
2020-01-07 20:14:40 -0600

    Done with initial functionality

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+const burger = (props) => {
+  //transfoming the props.ingredient object to an array as per required quintity.
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    }) // adding reduce method to make an empty array if no igrediants are there.
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
