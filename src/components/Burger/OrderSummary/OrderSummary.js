f1d9bd02f9bc423fb8f1c200afc079e8769c39e3
abhi <abrahmbhatt03@gmai.com>
2020-01-09 14:09:14 -0600

    Added toolbar and order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Auxiliary';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
@@ -16,7 +17,16 @@ const orderSummary = (props) => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total price:{props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to checkout</p>
+      <Button btnTypes="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnTypes="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
     </Aux>
   );
 };
