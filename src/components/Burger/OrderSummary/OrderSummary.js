1a976eab7aa67396f3a2da50ab03294bb498967e
abhi <abrahmbhatt03@gmai.com>
2020-01-23 14:30:15 -0600

    Done with the front end part

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,38 @@
 import React from 'react';
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
+class OrderSummary extends React.Component {
+  render() {
+    const { props } = this;
+
+    const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:
+          {props.ingredients[igKey]}
+        </li>
+      );
+    });
+
     return (
-      <li key={igKey}>
-        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:
-        {props.ingredients[igKey]}
-      </li>
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total price:{props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to checkout</p>
+        <Button btnTypes="Danger" clicked={props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnTypes="Success" clicked={props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Aux>
     );
-  });
+  }
+}
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total price:{props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to checkout</p>
-      <Button btnTypes="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnTypes="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Aux>
-  );
-};
-
-export default orderSummary;
+export default OrderSummary;
